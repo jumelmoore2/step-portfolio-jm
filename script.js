2e21d00de6556c6f2a6a7fc008dc5de4a8fb2f80
jumelmoore2 <jmthehaha2009@gmail.com>
2025-07-29 19:22:25 +0000

    add java script components

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log("hello world");
+let chicken =123;
+console.log(chicken);
+console.log(typeof chicken);9
+
+console.log(10+ "eggs")
+console. log (10+8+)
+
+console. log(5 > 2)
+console.log(7>3)&&(10>8);
+
+
+
+//only
+console.log(10>3)||(5>10);
+
+//if the statement is
+console.log(!5>3);
+
+const hour =new date().gethours().gethours
+greeting = "good morning"
+}else if (hour<18){
+    greeting = "good afternoon"
+    
+} greeting =
+
+
+document.getElementsByid("greeting") = greeting
